da94f5c0d806c5fa118d0db9de21e0ad25fbf75f
Buzzertech <prasadnayak1006@gmail.com>
2020-06-07 23:58:10 +0530

    feat: add tests for image scout

--- lib/image_scout/scout.ex
+++ lib/image_scout/scout.ex
@@ -3,27 +3,7 @@ defmodule Musikbot.ImageScout.Scout do
 
   alias Musikbot.ImageScout.{Unsplash}
 
-  @impl true
-  def init(:ok), do: {:ok, %{}}
-
-  @impl true
-  def handle_cast({:fetch_random_image}, state) do
-    case Unsplash.fetch_image_from_unsplash() do
-      {:ok, image_data} ->
-        {:noreply, %{state | image_data: image_data}}
-
-      {:error, error} ->
-        IO.puts(error)
-
-      {:fatal_error, error} ->
-        IO.puts(error)
-    end
-  end
-
-  @impl true
-  def handle_call({:get_image_state}, _from, state) do
-    {:reply, Map.fetch!(state, :image_data), state}
-  end
+  def start_link(opts), do: GenServer.start_link(__MODULE__, :ok, opts)
 
   def save_to_disk(svg_string), do: Temp.open!("image.svg", &IO.write(&1, svg_string))
 
@@ -34,11 +14,34 @@ defmodule Musikbot.ImageScout.Scout do
   end
 
   def get_image_state(server) do
-    {:ok, state} = GenServer.call(server, {:get_image_state}, 5000)
-    state
+    GenServer.call(server, {:get_image_state}, 5000)
   end
 
   @spec get_svg_from_template(%{track_data: map, image_data: map}) :: String.t()
   def get_svg_from_template(template_data),
     do: Mustache.render(Application.get_env(:musikbot, :misc)[:svg_template], template_data)
+
+  @impl true
+  def init(:ok), do: {:ok, %{}}
+
+  @impl true
+  def handle_cast({:fetch_random_image}, state) do
+    case Unsplash.fetch_image_from_unsplash() do
+      {:ok, image_data} ->
+        {:noreply, %{image_data: image_data}}
+
+      {:error, error} ->
+        IO.puts(error)
+        throw(error)
+
+      {:fatal_error, error} ->
+        IO.puts(error)
+        throw(error)
+    end
+  end
+
+  @impl true
+  def handle_call({:get_image_state}, _from, state) do
+    {:reply, Map.fetch!(state, :image_data), state}
+  end
 end
